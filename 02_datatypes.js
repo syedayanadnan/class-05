52a269dec80820a8fbd28b4ccb39b733a9c6ada8
syedayanadnan <youremail@example.com>
2024-02-20 12:48:16 +0500

    first commit

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use Strict" // treat all JS code as newer version
+//alert( 2+2) // we are using Node.JS ,not browser
+console.log(3
+    +3);// code readability should be high
+    console.log("syed ayan");
+    //  The documantation for jawascript (ECMA script {tc 39.es, MDN})
+    let name ="syed umar shah"
+    let age = 16 
+    let isLoggedIn = true
+    let State;
+    // number => 2 to power 53
+    //bignit
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined =>
+    //symbol => unique
+    //objects
+    console.log(typeof null);//object
+    console.log(typeof Object);//function
+    console.log(typeof undefined);//undefined
